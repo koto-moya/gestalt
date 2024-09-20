990111a179ecf5c941c4f7dce57150c5d8519717
koto-moya <kotogenshi@gmail.com>
2024-09-20 16:05:00 -0700

    included favicon

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from fastapi import FastAPI
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from fastapi import Request
-from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import RedirectResponse
 
 from config import limiter
 from routers import items, system_status, user_management
@@ -17,6 +16,8 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.include_router(items.router)
 app.include_router(system_status.router)
 app.include_router(user_management.router)
+
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 @app.get("/")
 def landing_page(request: Request):
